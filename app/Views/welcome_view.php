b9e3e0a3b211f4ce5479ead52b07349401a1db34
parab.smita <parab.smita.s11@gmail.com>
2024-10-08 15:20:21 +0530

    poject setup

---
+++ app/Views/welcome_view.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome to CodeIgniter 4!</title>
+    <meta name="description" content="The small framework with powerful features">
+</head>
+<body>
+    <h1>hello CI 4 </h1>
+</body>
+</html>
